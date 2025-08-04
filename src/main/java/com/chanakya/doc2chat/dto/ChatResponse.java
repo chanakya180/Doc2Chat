bb23ac6aa60fea8515f6b317a6db576545cb1776
Chanakya <chanakya.n@ascentbusiness.com>
2025-08-04 14:28:01 +0530

    feat: started frontend using angular

--- src/main/java/com/chanakya/doc2chat/dto/ChatResponse.java
+++ src/main/java/com/chanakya/doc2chat/dto/ChatResponse.java
@@ -1,13 +1,12 @@
 package com.chanakya.doc2chat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class ChatResponse {
     private String response;
-    public ChatResponse(String response) {
-        this.response = response;
-    }
 }
